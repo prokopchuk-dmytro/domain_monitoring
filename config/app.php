3320fb239fa6ccdd4be9a785933c0fc6c6064cde
dmytro <prokopchuk.dima@gmail.com>
2026-03-03 20:40:02 +0200

    deploy

--- config/app.php
+++ config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
+    'cron_secret' => env('CRON_SECRET', ''),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
